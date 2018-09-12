b8b9616410696c4a34775dbed40db516715badbf
CubicPill <cubicpill@gmail.com>
2018-09-12 17:49:09 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+    name='wgcontroller32',
+    version='1.0.0',
+    description='微耕 32 位门禁系统二次开发 Python 版',
+    author='CubicPill',
+    author_email='cubicpill@gmail.com',
+    url='https://github.com/CubicPill/wg-controller32',
+    packages=['wgcontroller32'],
+
+)
